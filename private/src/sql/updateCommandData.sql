ce6ea0f1a556f06bf5addc7d184d7daba7f79e4b
Philipp Schuster <phip1611@gmail.com>
2016-10-15 23:06:29 +0200

    comment

--- private/src/sql/updateCommandData.sql
+++ private/src/sql/updateCommandData.sql
@@ -1,4 +1,5 @@
 UPDATE `%TABLE_PREFIX%%TABLE_NAME%`
 SET `command`=:command,
     `referenceDatetime`=NOW()/*Trick um bei gleich bleibendem Kommando die referenceDatetime (ON UPDATE CURRENT TIMESTAMP) ändern! */
+    /* Notwendig damit man mehrere "next" hintereinander ausführen kann */
 WHERE `id` = 1;
