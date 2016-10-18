70e1da030cc28a338aa396be945deca46339d43d
Philipp Schuster <phip1611@gmail.com>
2016-10-18 10:19:26 +0200

    comments

--- private/src/sql/insertDefaultData.sql
+++ private/src/sql/insertDefaultData.sql
@@ -1,15 +1,13 @@
 /*Die letzte Teile ist notwendig, damit eine Zeile mit den ganzen Standardwerten eingefügt wird*/
-INSERT INTO `%TABLE_PREFIX%%TABLE_NAME%` (`id`, `presentation_identifier`, `command`, `options`) VALUES (1, 'default', 'start'
+INSERT INTO `%TABLE_PREFIX%%TABLE_NAME%` (`id`, `presentation_identifier`, `command`, `options`) VALUES (1, 'default', 'start',
 '{"usePolling":true,"useRegularGet":true,"refreshRate":500,"allowUserNavigation":false,"followServerCommands":true}'
 );
 /*
 {
-    "options": {
-	    "usePolling": true,
-    	"useRegularGet": true,
-	    "refreshRate": 500,
-	    "allowUserNavigation": false,
-	    "followServerCommands": true
-    }
+    ""usePolling": true,
+    "useRegularGet": true,
+    "refreshRate": 500,
+    "allowUserNavigation": false,
+    "followServerCommands": true
 }
 */
