b2cceadb767e876fff6e2d9be154a439346b3dcf
Philipp Schuster <phip1611@gmail.com>
2016-10-22 00:06:22 +0200

    sinnlose schleifen entfernt

--- private/php/restGetPollingWebSlidesData.inc.php
+++ private/php/restGetPollingWebSlidesData.inc.php
@@ -14,18 +14,17 @@ while (true) {
     try {
         $stmt = $pdo->query($sql);
         // es gibt sowieso nur einen Datensatz bzw. nur die erste Zeile ist relevant
-        while ($row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
-            if ($restReferenceDatetime == -1) { // aller erster Durchlauf
-                $restReferenceDatetime = $row[0];
-            }
-            if ($restReferenceDatetime != $row[0]) {
-                $jsonData->setData("referenceDatetime", $row[0]);
-                $jsonData->setData("command", $row[1]);
-                $jsonData->setData("options", json_decode($row[2]));
-                $jsonData->setData("presentation-identifier", $row[3]);
-                $jsonData->setData("message", "success");
-                die($jsonData->getJsonAsString());
-            }
+        $row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT);
+        if ($restReferenceDatetime == -1) { // aller erster Durchlauf
+            $restReferenceDatetime = $row[0];
+        }
+        if ($restReferenceDatetime != $row[0]) {
+            $jsonData->setData("referenceDatetime", $row[0]);
+            $jsonData->setData("command", $row[1]);
+            $jsonData->setData("options", json_decode($row[2]));
+            $jsonData->setData("presentation-identifier", $row[3]);
+            $jsonData->setData("message", "success");
+            die($jsonData->getJsonAsString());
         }
     } catch (PDOException $ex) {
         $jsonData->setData("message", "error");
--- private/php/restGetRegularWebSlidesData.inc.php
+++ private/php/restGetRegularWebSlidesData.inc.php
@@ -7,12 +7,11 @@ try {
 
     $stmt = $pdo->query($sql);
     // es gibt sowieso nur einen Datensatz bzw. nur die erste Zeile ist relevant
-    while ($row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
-        $jsonData->setData("referenceDatetime", $row[0]);
-        $jsonData->setData("command", $row[1]);
-        $jsonData->setData("options", json_decode($row[2]));
-        $jsonData->setData("presentation-identifier", $row[3]);
-    }
+    $row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT);
+    $jsonData->setData("referenceDatetime", $row[0]);
+    $jsonData->setData("command", $row[1]);
+    $jsonData->setData("options", json_decode($row[2]));
+    $jsonData->setData("presentation-identifier", $row[3]);
     $jsonData->setData("message", "success");
     die($jsonData->getJsonAsString());
 } catch (PDOException $ex) {
